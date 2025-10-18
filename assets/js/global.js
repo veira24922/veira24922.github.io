776d31d6a63d2f7a7abbd29b2219233ea2045e6d
Veira24922 <robloxuser24922@gmail.com>
2025-10-18 19:34:04 -0300

    Remove screenshots.html, add projects.html, and that's it!

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,4 +1,7 @@
 window.redirectTo = function(str) {
     if (str == "home") window.location.href = "/";
-    if (str == "screenshots") window.location.href = "/screenshots.html";
+    if (str == "screenshots") window.location.href = "/projects.html";
+}
+window.redirectExt = function(str) {
+    if (str == "tubextend-userscript") window.location.href = "https://greasyfork.org/scripts/553035-tubextend-2013-2015-youtube-redux-add-on";
 }
